7c36e23ecf5d7c8552e574d87cba59f974b7d70d
stanley <stanley6453@gmail.com>
2023-02-28 21:09:30 +0100

    created file 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -11,22 +11,32 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	int lbst, rbst;
 
 	if (tree == NULL)
-		return(0);
+		return (0);
 
 	lbst = tree_is_bst(tree->left, tree->n, 0);
 	rbst = tree_is_bst(tree->right, tree->n, 1);
 	return (minnum(lbst, rbst));
 }
 
+/**
+ * tree_is_bst - checks if all the nodes in a tree are greater or less than
+ * a value based on the pos value.
+ * @tree: tree to check
+ * @value: value to compare to tree value
+ * @pos: pos of 0 means tree value has to be less than value.
+ *  pos of 1 means tree value has to be greater tan value
+ * Return: 1 if condition holds, 0 if not.
+ */
 int tree_is_bst(const binary_tree_t *tree, int value, int pos)
 {
-	int lbst = 1, rbst = 1 , c1bst = 1,  c2bst = 1;
+	int lbst = 1, rbst = 1, c1bst = 1,  c2bst = 1;
+
 	if (tree == NULL)
 		return (1);
 
 	lbst = tree_is_bst(tree->left, value, pos);
 	rbst = tree_is_bst(tree->right, value, pos);
-	
+
 	if (pos == 0)
 	{
 		c1bst = tree->n < value ? 1 : 0;
