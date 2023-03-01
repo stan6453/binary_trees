dd17538aa16ebf5f3da303f1c120fbb3a207ee30
stanley <stanley6453@gmail.com>
2023-03-01 15:50:47 +0100

    added file 113-bst_search.c

--- binary_trees.h
+++ binary_trees.h
@@ -61,12 +61,11 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 int tree_is_bst(const binary_tree_t *tree, int value, int pos);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *bst_insert(bst_t **tree, int value);
-bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_search(bst_t *tree, int value);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 bst_t *array_to_bst(int *array, size_t size);
-
 
 #endif
