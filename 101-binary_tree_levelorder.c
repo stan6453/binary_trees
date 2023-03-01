1836f1041c5a7ac45c481e40610ba3f15f992333
stanley <stanley6453@gmail.com>
2023-03-01 10:56:04 +0100

    modified file 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,15 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - goes through a binary tree using
+ * level-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. The value in the node must be passed as a parameter to this function.
+ * @func: pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function.
  * Return: If tree or func is NULL, do nothing
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	/*
-	 * for level order traversal, i used a stack.
+	 * for level order traversal, i used a queue.
 	 * where the node to be processed are taken from the front (lindex)
 	 * and the new nodes are added to the back (rindex)
 	 */
@@ -23,12 +25,12 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 
 	treelist[rindex++] = tree;
 
-	while(rindex > lindex)
+	while (rindex > lindex)
 	{
 		temp = treelist[lindex++];
-		if(temp->left != NULL)
+		if (temp->left != NULL)
 			treelist[rindex++] = temp->left;
-		if(temp->right != NULL)
+		if (temp->right != NULL)
 			treelist[rindex++] = temp->right;
 		func(temp->n);
 	}
