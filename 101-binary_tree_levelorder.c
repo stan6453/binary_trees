00dc752af9a27e3c8eea1ea23255fe809a31532f
stanley <stanley6453@gmail.com>
2023-03-01 10:54:50 +0100

    modified file 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -8,14 +8,28 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *treelist[100];
-	int index = 0;
-	binary_tree_t *temp;
+	/*
+	 * for level order traversal, i used a stack.
+	 * where the node to be processed are taken from the front (lindex)
+	 * and the new nodes are added to the back (rindex)
+	 */
+	const binary_tree_t *treelist[100], *temp;
+	int lindex = 0, rindex = 0;
 
 	if (tree == NULL)
 		return;
 	if (func == NULL)
 		return;
 
-	traverse_level(treelist, &index, tree, func);
+	treelist[rindex++] = tree;
+
+	while(rindex > lindex)
+	{
+		temp = treelist[lindex++];
+		if(temp->left != NULL)
+			treelist[rindex++] = temp->left;
+		if(temp->right != NULL)
+			treelist[rindex++] = temp->right;
+		func(temp->n);
+	}
 }
